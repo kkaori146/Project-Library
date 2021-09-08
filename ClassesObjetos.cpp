4224af476cb652a7406754097b772d6d96751589
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-08 14:04:44 +0900

    File Preparation

---
+++ ClassesObjetos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\nClasses e Objetos" << endl;
+
+	return 0;
+
+	system("PAUSE");
+}
---
+++ Livro.cpp
@@ -0,0 +1,8 @@
+//método
+
+#include "Livro.h"
+
+Livro::Livro() //Nome da classe :: Nome do método
+{
+
+}
