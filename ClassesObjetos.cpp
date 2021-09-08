cb086de601f41dba84a1da03f465fe9fcdf22848
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-08 15:17:23 +0900

    Book: Moby-Dick

--- ClassesObjetos.cpp
+++ ClassesObjetos.cpp
@@ -6,18 +6,41 @@ using namespace std;
 
 int main()
 {
-	cout << "\nClasses e Objetos" << endl;
-
 	Livro *meuLivro;
 	meuLivro = new Livro();
+	
+		meuLivro->codigo = 1;
+		strcpy(meuLivro->titulo, "The Little Prince");
 
-	meuLivro->codigo = 1;
-	strcpy(meuLivro->titulo, "The Little Prince");
+		cout << "(meuLivro) Codigo: " << meuLivro->codigo << endl;
+		cout << "(meuLivro) Titulo: " << meuLivro->titulo << endl;
 
-	cout << "(meuLivro) Codigo:" << meuLivro->codigo << endl;
-	cout << "(meuLivro) Titulo:" << meuLivro->titulo << endl;
+		delete meuLivro;//Liberar espaço de memória após a consulta
+	
+	unsigned int codigo;
+	char titulo[100];
+	char editora[60];
+	unsigned int paginas;
+	char isbn[20];
 
-	delete meuLivro;//Liberar espaço de memória após a consulta
+	codigo = 2;
+	strcpy(titulo, "Moby-Dick (2)");
+	strcpy(editora, "Richard Bentley");
+	paginas = 635;
+	strcpy(isbn, "9788575036709");
+
+	Livro *livroMobyDick;
+
+	livroMobyDick = new Livro(codigo, titulo, editora, paginas, isbn);
+
+	cout << endl;
+	cout << "(livroMobyDick) Codigo: " << livroMobyDick->codigo << endl;
+	cout << "(livroMobyDick) Titulo: " << livroMobyDick->titulo << endl;
+	cout << "(livroMobyDick) Editora: " << livroMobyDick->editora << endl;
+	cout << "(livroMobyDick) Paginas: " << livroMobyDick->paginas << endl;
+	cout << "(livroMobyDick) ISBN: " << livroMobyDick->isbn << endl;
+
+	delete livroMobyDick;
 
 	system("PAUSE");
 
--- Livro.cpp
+++ Livro.cpp
@@ -8,7 +8,7 @@ Livro::Livro() //Nome da classe :: Nome do método
 
 }
 
-Livro::Livro(unsigned int codigo, char titulo[], char editora[], unsigned int paginas, char isbn[]);
+Livro::Livro(unsigned int codigo, char titulo[], char editora[], unsigned int paginas, char isbn[])
 {
 	this->codigo = codigo;
 	strcpy(this->titulo, titulo);
