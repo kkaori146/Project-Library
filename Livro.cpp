ac6da4b3c0cf5f32c6a4b6a3fd3e6693b0f0b6db
Kaminishikawahara KK <kkaori146@gmail.com>
2021-09-08 14:36:44 +0900

    Livro Method: Builder

--- Livro.cpp
+++ Livro.cpp
@@ -1,8 +1,23 @@
 //método
 
 #include "Livro.h"
+#include <string>
 
 Livro::Livro() //Nome da classe :: Nome do método
+{
+
+}
+
+Livro::Livro(unsigned int codigo, char titulo[], char editora[], unsigned int paginas, char isbn[]);
+{
+	this->codigo = codigo;
+	strcpy(this->titulo, titulo);
+	strcpy(this->editora, editora);
+	this->paginas = paginas;
+	strcpy(this->isbn, isbn);
+}
+
+Livro::~Livro()
 {
 
 }
